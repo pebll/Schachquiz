b35c818ef650829b3eae0ff78fabb847a29f0eaf
pebll <siggi@brucker.fr>
2022-04-22 17:55:40 +0200

    fix + tile class

--- main.js
+++ main.js
@@ -16,13 +16,37 @@ var leider_viel_zu_früh_verglichen = 4 === 4;
 console.log(leider_viel_zu_früh_verglichen);
 */
 
-let randomFile = parseInt(Math.random()*8);
-let randomRank = parseInt(Math.random()*8);
+let randomFile = parseInt(Math.random() * 8);
+let randomRank = parseInt(Math.random() * 8);
 
-let files = ["a","b","c","d","e","f","g","h"]; 
-let ranks = ["1","2","3","4","5","6","7","8","9"];
 
-let randomsquare = (String(files[randomFile]) + String( ranks[randomRank]));
-alert("Wohin kann der Springer auf "+randomsquare+  " ?");
+
+let randomsquare = (String(files[randomFile]) + String(ranks[randomRank]));
+alert("Wohin kann der Springer auf " + randomsquare + " ?");
+
+function checkLegalHorseeMove(tile1_x, tile2) {
+
+}
+
+class Tile {
+
+    // file & rank in integers, to String method prints out the actual name of the tile
+    constructor(file, rank) {
+        this.file = file;
+        this.rank = rank;
+    }
+    constructor() {
+        this.file = files;
+        this.rank = rank;
+    }
+
+    print() {
+        let files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+        let ranks = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        return String(files[file]) + String(rank[rank])
+    }
+}
+
+
 
 
