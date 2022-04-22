8382a6127885425612bc609b47c7df6a8e9d5adc
pebll <siggi@brucker.fr>
2022-04-22 18:31:24 +0200

    tile class now works :)

--- main.js
+++ main.js
@@ -17,17 +17,12 @@ console.log(leider_viel_zu_früh_verglichen);
 */
 
 
-//let randomsquare = new Tile();
-alert("test")
-alert("Wohin kann der Springer auf " + randomsquare.print() + " ?");
-
-/**
 class Tile {
     // file & rank in integers, to String method prints out the actual name of the tile
-    constructor(file, rank) {
+    /**constructor(file, rank) {
         this.file = file;
         this.rank = rank;
-    }
+    }*/
     constructor() {
         this.file = parseInt(Math.random() * 8);
         this.rank = parseInt(Math.random() * 8);
@@ -36,13 +31,13 @@ class Tile {
     print() {
         let files = ["a", "b", "c", "d", "e", "f", "g", "h"];
         let ranks = ["1", "2", "3", "4", "5", "6", "7", "8"];
-        return String(files[file]) + String(rank[rank])
+        return String(files[this.file]) + String(ranks[this.rank]);
     }
 }
-*/
-//yeet
 
 
+let randomsquare = new Tile();
+alert("test");
+alert("Wohin kann der Springer auf " + randomsquare.print() + " ?");
 
 
-//hui
