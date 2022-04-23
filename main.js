284d4e19bb4b65bb9aede202a033579d806d8df7
Georg Pernice <georgpernice@gmail.com>
2022-04-23 15:59:34 +0200

    Jetzt prüft es die Nutzereingabe gescheit

--- main.js
+++ main.js
@@ -67,26 +67,27 @@ function tileListToString(list) {
     return string;
 }
 
-
+let rightOption = 0; //globale Variable
 
 function generateQuestion(startSquare){
-    let rightOption = Math.floor(Math.random()*3)+1; //zufallszahl zw 1 und 3
+    rightOption = Math.floor(Math.random()*3); //zufallszahl zw 0 und 2
     let random = Math.floor(Math.random()*8); //zz zw 0 7
-    
-
     for(let i = 0; i<3; i++){// in schleife schreibe die optionen in das HTML
         if(i != rightOption){ //weise zufallssquare zu, (dass aber auch ein legal square sein kan BUG!!!)
-        document.getElementById(i+1).innerHTML = '<input type="radio" name="felder" id = "11" value="option1">'+new Tile().print()+'</input><br/>';
+        document.getElementById(i).innerHTML = '<input type="radio" name="felder" value="option'+String(i)+'">'+new Tile().print()+'</input><br/>';
         }
         else{ //die korrekte Zugmöglichkeit
-            document.getElementById(i+1).innerHTML = '<input type="radio" name="felder" id = "11" value="option1">'+startSquare.getKnightMoveTiles()[random].print()+'</input><br/>';
+            document.getElementById(i).innerHTML = '<input type="radio" name="felder" value="option'+String(i)+'">'+startSquare.getKnightMoveTiles()[random].print()+'</input><br/>';
         }
     }
 }
 /**checkAnswer() wird direkt nach dem buttonclick ausgeführt */
 function checkAnswer(){
-    //let antwort = document.querySelector('input[name="felder"]:checked').id; //id ist 1,2 oder 3
-    alert("Submitted");
+    let antwort = document.querySelector('input[name="felder"]:checked').value; //value ist option1,option2 oder option3
+    alert("Submitted " + antwort);
+    if(antwort[6]==String(rightOption)){
+        alert("Right!");
+    }
 }
 
 
