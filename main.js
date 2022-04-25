cc49037e0e25659b23d92070b85251f33d7166ae
Georg Pernice <georgpernice@gmail.com>
2022-04-25 23:33:15 +0200

    Modulare Bauweise

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import {Tile} from "./Tile.js";
+import {Game} from "./Game.js";
 'use strict';
 "use collections/list";
 
@@ -17,62 +18,6 @@ kommt eine meldung "richtig"
 
 
 
-class Game {
-    constructor(points = 0, level = 0, levels = ["Novize", "Amateur", "Dude aus dem Achten", "Luca", "Dude aus dem Dritten"],
-        difficultyArray = [1, 2, 4, 5, 6], colors = ["white", "red", "blue", "green", "purple"]) {
-        this.points = 0;
-        this.level = 0;
-
-        if (difficultyArray.length == levels.length) {
-            this.levels = levels;
-            this.difficultyArray = difficultyArray;
-            this.colors = colors;
-        }
-        else {
-            alert("Fehler in class 'Game': es müssen gleich viele Punktestände zum aufleveln wie level da sein.");
-            this.levels = ["DefaultNovize", "Amateur", "Clubspieler", "Luca", "der Typ aus dem dritten"];
-            this.difficultyArray = [1, 2, 3, 5, 6];
-            this.colors = ["white", "red", "blue", "green", "purple"];
-        }
-    }
-    levelToString() {
-        if (this.level < this.levels.length) {
-            return this.levels[this.level];
-        }
-        else {
-            return "EsGibtKeinMaßFürdeineIntelligenz"
-        }
-    }
-    updateLevel() {
-        let notUpToDate = this.difficultyArray[this.level] < this.points;
-        let noMoreLevels = this.points > (this.difficultyArray[this.levels.length] + 1);
-        if (notUpToDate & !noMoreLevels) {
-            this.level++;
-            this.updateLevel();
-        }
-    }
-    getLevelColor() {
-
-        if (this.level < this.levels.length) {
-            return this.colors[this.level];
-        }
-        else {
-            return "white";
-        }
-    }
-    addPoint() {
-        this.points++;
-        this.updateLevel();
-    }
-}
-function tileListToString(list) {
-    let string = "";
-    for (let i = 0; i < list.length; i++) {
-        string += list[i].print();
-        string += " ";
-    }
-    return string;
-}
 
 
 
@@ -81,7 +26,7 @@ let rightOption = 0;
 let randomStartSquare = new Tile(0, 0, true);
 let game = new Game(0, 0);
 //------------------------------------------
-function generateQuestion() {
+export function generateQuestion() {
     rightOption = Math.floor(Math.random() * 3); //zufallszahl zw 0 und 2
 
     for (let i = 0; i < 3; i++) {// in schleife schreibe die optionen in das HTML
@@ -96,7 +41,7 @@ function generateQuestion() {
 function updateTheme() {
     document.getElementsByClassName("triggeredByLevelUp")[0].style.color = game.getLevelColor();
 }
-function updateText() {
+export function updateText() {
     document.getElementById("aufgabe").innerText = "Der Springer steht zunächst auf " + randomStartSquare.print() + ".";
     document.getElementById("punkte").innerText = "Punkte: " + String(game.points);
     document.getElementById("rang").innerText = "Rang: " + game.levelToString();
